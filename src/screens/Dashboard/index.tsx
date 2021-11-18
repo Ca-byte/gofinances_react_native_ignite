ea902093e6655bb2a3b311848b6e5f25270ffa5b
Caroline Vieira <carolinesanvieira@gmail.com>
2021-11-18 23:06:02 +0100

    Styled Components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+import { Container, Title } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
