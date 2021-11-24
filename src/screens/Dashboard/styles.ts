5e1ed9696ea7b2f2a4a45d5e02b1c7cecc5d246f
Caroline Vieira <carolinesanvieira@gmail.com>
2021-11-24 22:52:14 +0100

    Highlight Cards

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components/native";
 
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
 import { Feather } from "@expo/vector-icons";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
@@ -10,13 +12,20 @@ export const Container = styled.View`
 export const Header = styled.View`
   width: 100%;
   height: ${RFPercentage(42)}px;
+
   background-color: ${({ theme }) => theme.colors.primary};
+
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  flex-direction: row;
 `;
 export const UserWrapper = styled.View`
   width: 100%;
+
   padding: 0 24px;
+
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
+
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -61,4 +70,9 @@ export const HighlightCards = styled.ScrollView.attrs({
   horizontal: true,
   showsHorizontalScrollIndicator: false,
   contentContainerStyle: { paddingHorizontal: 24 },
-})``;
+})`
+  width: 100%;
+
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
+`;
